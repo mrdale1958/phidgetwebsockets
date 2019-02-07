60769101608fd0111b71e6b9dd8ea4ae82b40d45
mrdale1958 <dale.macdonald@utdallas.edu>
2019-02-07 11:01:53 -0600

    bad indent

--- python/TwistData.py
+++ python/TwistData.py
@@ -48,15 +48,15 @@ class TwistData:
             d = {'clientip': "twister", 'user':"__init__"}
             TwistData._logger.critical('_twister init failed: %s', 'details%s'% e.details, extra=d)
             TwistData._twister = None
-            for sensorNum in range(2):
-                try:
-                    TwistData._twister[sensorNum].openWaitForAttachment(TwistData._waitTimeForConnect)
-                    TwistData._twister[sensorNum].setDataInterval(TwistData._twister[sensorNum].getMinDataInterval())
-                    TwistData._logger.info('_twister connecting: %s', 'details channel: %d '% (sensorNum), extra={})
-                except PhidgetException as e:
-                    d = {'clientip': "twister", 'user':"open"}
-                    TwistData._logger.critical('_twister connect failed: %s', 'details channel: %d error: %s'% (sensorNum, e.details), extra=d)
-                    TwistData._twister = None
+        for sensorNum in range(2):
+            try:
+                TwistData._twister[sensorNum].openWaitForAttachment(TwistData._waitTimeForConnect)
+                TwistData._twister[sensorNum].setDataInterval(TwistData._twister[sensorNum].getMinDataInterval())
+                TwistData._logger.info('_twister connecting: %s', 'details channel: %d '% (sensorNum), extra={})
+            except PhidgetException as e:
+                d = {'clientip': "twister", 'user':"open"}
+                TwistData._logger.critical('_twister connect failed: %s', 'details channel: %d error: %s'% (sensorNum, e.details), extra=d)
+                TwistData._twister = None
 
 
     def ingestTwistData(self, channel, positionChange, time):
